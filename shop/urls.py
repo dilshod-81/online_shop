20a9314fa5b51c7bece6ec18fad9610d1c0b83f0
Dilshod <murodkobilovdilshod@mail.ru>
2022-08-24 01:02:38 +0500

    ...

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from  .views import ShopView
+from  .views import ShopView, ProductDetailView
 
 app_name = 'shop'
 urlpatterns = [
-    path('', ShopView.as_view(), name='home')
+    path('', ShopView.as_view(), name='home'),
+    path('/product/<int:pk>/', ProductDetailView.as_view(), name='detial')
 ]
