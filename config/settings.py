f33eca39bc05ea0b1bbc5a717abea92c034f8e44
Dilshod <murodkobilovdilshod@mail.ru>
2022-08-29 07:10:45 +0500

    finish filter sidebar

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            'libraries': {
+                'my_templatetag': 'shop.templatetags.my_tags',
+            }
         },
     },
 ]
