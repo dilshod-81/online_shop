0528b8f58f5e1bfa70139a92cd3beb1bbdc0dd23
Dilshod <murodkobilovdilshod@mail.ru>
2022-09-18 23:29:02 +0500

    profile done

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from  .views import ShopView, ProductDetailView, wishlist_view, WishlistListView, update_cart_view
+from  .views import ShopView, ProductDetailView, wishlist_view, WishlistListView, update_cart_view, ShoppingCartView
 
 app_name = 'shop'
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detial'),
     path('product/<int:pk>/wishlist/', wishlist_view, name='wishlist'),
     path('wishlists/', WishlistListView.as_view(), name='all_wishlist'),
-    path('add_cart/<int:id>/', update_cart_view, name='cart')
+    path('add_cart/<int:id>/', update_cart_view, name='cart'),
+    path('shopping_cart/', ShoppingCartView.as_view(), name='shopping_cart')
 
 ]
--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,14 @@
 from  django import forms
-from  .models import UserModel
+from .models import UserModel, ProfileModel
 from  django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 
+class ProfileForm(forms.ModelForm):
 
+    class Meta:
+        model = ProfileModel
+        exclude = ['user', 'created_at']
 
 class LoginForm(forms.Form):
     phone_number = forms.CharField(widget=forms.TextInput(), label=_('Phone number'))
